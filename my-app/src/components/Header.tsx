d1086225e6722c34cc0cb457073b7d4356a5807e
Anastasia-Yukhno <nasta5.666666@gmail.com>
2021-12-29 13:47:18 +0200

    fix header syntax

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {AllBtn} from "./AllBtn";
 import {FavoritesBtn} from "./FavoritesBtn";
 import {CartBtn} from "./CartBtn";
@@ -11,10 +10,10 @@ export const Header = () => {
         <HeaderContainer>
             <Logo>Donuts</Logo>
             <GroupOfLinks>
-                <AllBtn {...links}></AllBtn>
-                <FavoritesBtn {...links}></FavoritesBtn>
+                <AllBtn {...links}/>
+                <FavoritesBtn {...links}/>
             </GroupOfLinks>
-            <CartBtn {...links}></CartBtn>
+            <CartBtn {...links}/>
 
         </HeaderContainer>
     )
