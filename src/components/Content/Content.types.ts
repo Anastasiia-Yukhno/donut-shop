e799a01bd39752093efdaf08a59c07104b69cf92
Anastasia-Yukhno <nasta5.666666@gmail.com>
2021-12-31 16:56:24 +0200

    move Card component to Components folder

--- src/components/Content/Content.types.ts
+++
@@ -1,8 +0,0 @@
-export type TCard = {
-    title: string;
-    image: string;
-    price: number;
-    onBuy: (date:void) => void;
-    onLike: (date:void) => void;
-}
-
--- src/pages/All/All.tsx
+++ src/pages/All/All.tsx
@@ -1,17 +1,54 @@
 import { Header } from '../../components';
 import {links} from "../../contsts";
 import { Container } from './All.styles';
-import Content from "../../components/Content";
-
+import {useState} from "react";
+import {DonutList} from "../../Mock/donutList";
+import CardsItem from "../../components/Card";
+import {
+    CardsGroup,
+    CardsContainer,
+    ContainerAll,
+    HeaderAll,
+    Sorting,
+    TopPart,
+    Border,
+    SortButton,
+    Select,
+    Card,
+    CardsFooter,
+    CardsHeader,
+    Image,
+    CardsImageContainer,
+    Title,
+    CardsDescription, Price, CardsButtons, Like, Buy, Option, OptionLink
+} from './All.styles';
 const All = () => {
-  return (
-    <Container>
-      <Header
-        links={links}
-      />
-      <Content/>
-    </Container>
-  )
+    const [isSorting, setSorting] = useState(false)
+    return (
+        <Container>
+            <Header
+                links={links}
+            />
+            <ContainerAll>
+            <Border/>
+            <TopPart>
+            <HeaderAll>All</HeaderAll>
+            <Sorting>
+            <SortButton onClick={() => setSorting(!isSorting)}>Sorting</SortButton>
+        {isSorting && <Select>
+            <Option> <OptionLink>Featured</OptionLink></Option>
+            <Option> <OptionLink>Price, High to Low</OptionLink></Option>
+            <Option> <OptionLink>Price, Low to High</OptionLink></Option>
+            </Select>}
+            </Sorting>
+            </TopPart>
+            <CardsGroup>
+                {DonutList.map((card) => <CardsItem {...card} key={`${card.title} + ${Math.random()}`}/>)}
+            </CardsGroup>
+            </ContainerAll>
+
+        </Container>
+    )
 };
 
 export default All;
