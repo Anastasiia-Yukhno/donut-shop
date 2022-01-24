822475aa9586537714edeb6f5fe21dc356cb6b01
Anastasia-Yukhno <nasta5.666666@gmail.com>
2022-01-24 12:48:53 +0200

    fix popup visibility and styles

--- src/components/Popup/Popup.styles.tsx
+++ src/components/Popup/Popup.styles.tsx
@@ -2,21 +2,23 @@ import styled from 'styled-components'
 
 export const Select = styled.div`
     position: absolute;
-    z-index: 99;
+    z-index: 95;
     margin: 5px;
     font-size: 1em;
 `
 export const Option = styled.div`
     padding: 3px 0;
-    display: flex;
+    position: relative;
 `
-export const SortToolTip = styled.div`
+export const SortToolTip = styled.span`
     padding-left: 5px;
     font-size: 1em;
     font-family: monospace;
     border: none;
     cursor: pointer;
-    max-width: 150px;
+    position: absolute;
+    top: 0;
+    left: 45px;
 `
 export const OptionLink = styled.a`
     padding: 5px;
@@ -24,6 +26,7 @@ export const OptionLink = styled.a`
     background: #f0f0f0;
     font-size: 1.25em;
     text-align: center;
+    min-width: 30px;
     &:hover {
         background: #d6d6d6;
     }
